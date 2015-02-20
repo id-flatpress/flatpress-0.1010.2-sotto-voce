3fff7cbb7cafccdcbd7910b484ad7d745a7ccac6
Baguz A <eggoez@users.noreply.github.com>
2015-02-20 07:45:28 +0700

    Create lang.admin.uploader.php

---
+++ flatpress/fp-interface/lang/id-id/lang.admin.uploader.php
@@ -0,0 +1,35 @@
+<?php
+/* 
+# Diterjemahkan oleh eggoez - http://goez.my.id
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# Silahkan laporkan lewat e-mail di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+
+	$lang['admin']['uploader']['default'] = array(
+		'head'		=> 'Pengunggahan',
+		'descr'		=> 'Ambil satu atau lebih file untuk di unggah.',
+		'fset1'		=> 'File Terpilih',
+		'fset2'		=> 'Unggah',
+		'submit'	=> 'Unggah',
+
+	);
+
+	$lang['admin']['uploader']['default']['msgs'] = array(
+		1	=> 'File terpilih berhasil di unggah',
+		-1	=> 'Nampaknya ada kesalahan, silahkan Anda coba lagi',
+	);
+	
+	
+	
+	$lang['admin']['uploader']['browse'] = array(
+		'head'		=> 'Cari',
+		'descr'		=> 'Ambil satu atau lebih file untuk di unggah.',
+		'fset1'		=> 'File Dipilih',
+		'submit'	=> 'Unggah',
+
+	);
+
+	
+?>
