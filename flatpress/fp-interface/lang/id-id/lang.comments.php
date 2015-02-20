8725da3c3d77eed20b45554bfac73f27679d82de
Baguz A <eggoez@users.noreply.github.com>
2015-02-20 07:46:32 +0700

    Create lang.comments.php

---
+++ flatpress/fp-interface/lang/id-id/lang.comments.php
@@ -0,0 +1,27 @@
+<?php
+/* 
+# Diterjemahkan oleh eggoez - http://goez.my.id
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# Silahkan laporkan lewat e-mail di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+	$lang['comments']['mail'] = <<<MAIL
+Yang terhormat %toname%,
+
+"%fromname%" %frommail% telah menambahkan sebuah komentar pada entri "%entrytitle%".
+
+Ini adalah sebuah link alamat entri:
+%commentlink%
+
+disini komentar sudah diposting:
+***************
+%content%
+***************
+
+Semua yang terbaik,
+%blogtitle%
+
+MAIL;
+
+?>
