b7dffbccf4f69f724b03e5ee5298643d09249261
Baguz A <eggoez@users.noreply.github.com>
2015-02-20 07:43:26 +0700

    Create lang.admin.php

---
+++ flatpress/fp-interface/lang/id-id/lang.admin.php
@@ -0,0 +1,40 @@
+<?php
+/* 
+# Diterjemahkan oleh eggoez - http://goez.my.id
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# Silahkan laporkan lewat e-mail di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+	$lang['admin']['head'] = 'Admin Panel';
+	
+	$lang['admin']['panels'] = array(
+		'main'		=> 'Pilihan',
+		'config'	=> 'Pengaturan',
+		'users'		=> 'Pengguna',
+		'entry'		=> 'Entri',
+		'static'	=> 'Statis',
+		'uploader'	=> 'Unggah',
+		'widgets'	=> 'Widget',
+		'add'		=> 'Tambah',
+		'edit'		=> 'Ubah/Hapus',
+		'maintain'	=> 'Pelihara',
+		'plugin'	=> 'Plugin',
+		'themes'	=> 'Tema',
+		'updates'	=> 'Pembaruan',
+	);
+	
+	
+	$lang['admin']['general'] = array(
+		'id'		=> 'Id',	# for entry ids
+		'name'		=> 'Nama', 	# for file names
+		'title'		=> 'Judul', # for entry/posts/static titles
+		'comments'	=> 'Komentar',
+		'author'	=> 'Penulis',
+		'actdel'	=> 'Hapus',
+		'actedit'	=> 'Ubah',
+		'actenable'	=> 'Aktifkan',
+		'actdisable'=> 'Matikan'
+	);
+
+?>
