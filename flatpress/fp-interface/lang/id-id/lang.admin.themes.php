7eee00ef1c545e7f2b70ea430339af2fe7169851
Baguz A <eggoez@users.noreply.github.com>
2015-02-20 07:44:59 +0700

    Create lang.admin.themes.php

---
+++ flatpress/fp-interface/lang/id-id/lang.admin.themes.php
@@ -0,0 +1,54 @@
+<?php
+/* 
+# Diterjemahkan oleh eggoez - http://goez.my.id
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# Silahkan laporkan lewat e-mail di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+	
+	$lang['admin']['themes']['submenu'] = array(
+		'default'	=> 'Tema',
+		'style'		=> 'Gaya',
+	);
+	
+	/* default panel action (change theme) */
+	
+	$lang['admin']['themes']['default'] = array(
+		'head1'		=> 'Sedang dipakai',
+		
+		'head2'		=> 'Tema tersedia',
+		'descr'		=> 'Pilih sebuah tema untuk memakai',
+		
+		'nodescr'	=> 'Tidak ada keterangan',
+		'noauthor'	=> 'Pembuat tidak diketahui',
+
+	);
+	
+	$lang['admin']['themes']['default']['msgs'] = array(
+		1 => 'Tema telah dirubah',
+		-1 => 'Nampaknya ada kesalahan sehingga tidak tersimpan, silahkan ulangi kembali.',
+		-2 => 'Nampaknya tema yang Anda pilih tidak tersedia.'
+	);
+	
+	
+	/* select style */
+	
+	$lang['admin']['themes']['style'] = array(
+		'head1'		=> 'Anda memakai',
+		
+		'head2'		=> 'Tersedia gaya',
+		'descr'		=> 'Pilih gaya untuk meng-aktifkan',
+		
+		'nodescr'	=> 'Tidak ada keterangan',
+		'noauthor'	=> 'Tidak diketahui',
+	);
+	
+	$lang['admin']['themes']['style']['msgs'] = array(
+		1 => 'Style berhasil disimpan',
+		-1 => 'Nampaknya ada kesalahan sehingga tidak tersimpan, silahkan ulangi kembali..',
+		-2 => 'Nampaknya tema yang Anda pilih tidak tersedia.'
+	);
+	
+	
+?>
