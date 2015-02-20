6d3bff6576f2a4ec21b3fe9ebfb2cdfb66f86c8e
Baguz A <eggoez@users.noreply.github.com>
2015-02-20 07:47:08 +0700

    Create lang.conf.php

---
+++ flatpress/fp-interface/lang/id-id/lang.conf.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+# Diterjemahkan oleh eggoez - http://goez.my.id
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# Silahkan laporkan lewat e-mail di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+	$langconf = array();
+	$langconf['id'] = 'Bahasa Indonesia';
+	$langconf['locale'] = 'id-id';
+	$langconf['charsets'][0] = 'utf-8';
+	$langconf['charsets'][1] = 'iso-8859-15';
+
+?>
