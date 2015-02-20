6429479d8e002affd8fdac7c4d3bf0e298ba1c24
Baguz A <eggoez@users.noreply.github.com>
2015-02-20 07:47:35 +0700

    Create lang.contact.php

---
+++ flatpress/fp-interface/lang/id-id/lang.contact.php
@@ -0,0 +1,37 @@
+<?php
+/* 
+# Diterjemahkan oleh eggoez - http://eg.goez.my.id
+# Bila ada kesalahan baik secara penulisan atau ucapan kata
+# Silahkan laporkan lewat e-mail di eggoez@yahoo.com
+# Hargailah Karya Orang Lain dengan tidak Merubah Isi &
+# Terimakasih sudah menggunakan Bahasa Indonesia :)
+*/
+	$lang['contact'] = array(
+		'head'		=> 'Kontak',
+		'descr'		=> 'Silahkan isi formulir di bawah ini untuk mengirimkan umpan balik. Silahkan tambahkan e-mail Anda jika Anda ingin menjawab.',
+		'fieldset1'	=> 'Data pengguna',
+		'name'		=> 'Nama (*)',
+		'email'		=> 'Email:',
+		'www'		=> 'Website:',
+		'cookie'	=> 'Ingat saya',
+		'fieldset2'	=> 'Pesan',
+		'comment'	=> 'Komentar (*):',
+		'fieldset3'	=> 'Kirim',
+		'submit'	=> 'Kirim',
+		'reset'		=> 'Bersihkan',
+		
+	);
+	
+	$lang['contact']['error'] = array(
+		'name'		=> 'Anda harus masukkan nama',
+		'email'		=> 'Anda harus masukkan e-mail yang benar',
+		'www'		=> 'Masukan URL yang benar',
+		'content'	=> 'Masukan sebuah konten',
+	);
+	
+	$lang['contact']['msgs'] = array(
+		1	=> 'Pesan Anda sudah terkirim',
+		-1	=> 'Pesan Anda gagal dikirim',
+	);
+	
+?>
